0afbd6ea4d25dba45e1f3141e1da4253e77914e5
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-09-18 13:24:48 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1]=="."
+      true
+    else false
+    end
   end
 
   def question?
-
+    if self[-1]=="?"
+      true
+    else false
+    end
   end
 
   def exclamation?
-
+    if self[-1]=="!"
+      true
+    else false
+    end
   end
 
   def count_sentences
-
-  end
+    sentences = self.split(/[.?!]/).select do |sentence|
+      sentence.length > 0
+    end
+    sentences.length
+   end
 end
